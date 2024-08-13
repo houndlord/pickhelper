a7daa03d220d8e08a764b4e6eb4c201a64ad0ea2
Lev Chechulin <l.e.chechulin@gmail.com>
2024-08-13 16:53:26 +0200

    fix: fix api url.....

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://pickhelper.lol/';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://pickhelper.lol/api';
 
 export async function getAllChampions() {
   try {
